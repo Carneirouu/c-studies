bd015083dbdcd03bf9b0069a680d12471c7897e1
Matheus Carneiro <carneiromatheus943@gmail.com>
2026-07-06 18:31:44 -0300

    Fix logic errors

--- 05_Fibonacci_serie/fibonacci_serie.c
+++ 05_Fibonacci_serie/fibonacci_serie.c
@@ -13,20 +13,25 @@ void aureaProportion(int a, int b)
         printf("undefined\n");
     }
 }
-int main()
+void fibonacci(int num)
 {
-    int i, f = 0, c1 = 1, c2, num;
-
-    printf("How long Fibonacci serie?: "); // Choose how long will be the serie.
-    scanf("%d", &num);
-
+    int i, f = 0, c1 = 1, c2;
     for (i = 1; i <= num; i++) // Repete the block based on the lenght of the sequence.
     {
         printf("%d   ", f);
+        aureaProportion(f, c1); // Execute the aurea block.
+
         c2 = c1 + f;
-        aureaProportion(c1, f); // Execute the aurea block.
         c1 = f;
         f = c2;
     }
+}
+int main()
+{
+    int num;
+
+    printf("How long Fibonacci serie?: "); // Choose how long will be the serie.
+    scanf("%d", &num);
+    fibonacci(num);
     return 0;
 }
