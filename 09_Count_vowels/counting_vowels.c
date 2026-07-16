e87b7b32295ccb0fc0955d22a9188ea9fa67c1c7
Matheus Carneiro <carneiromatheus943@gmail.com>
2026-07-16 08:23:54 -0300

    array features

--- 09_Count_vowels/counting_vowels.c
+++ 09_Count_vowels/counting_vowels.c
@@ -1,11 +1,27 @@
 #include <stdio.h>
+#include <string.h>
 
-int main ()
+int main()
 {
-    char * word;
-    int vowels;
+    char word[31], vowels[] = "aeiou";
+    int vowelCounter = 0, vowelIndex, charIndex;
 
-    scanf("%c", word);
-    
+    printf("Type a word: ");
+    scanf("%30s", word);
 
+    int length = strlen(word);
+
+    for (charIndex = 0; charIndex < length; charIndex++)
+    {
+        for (vowelIndex = 0; vowelIndex < 5; vowelIndex++)
+        {
+            if (word[charIndex] == vowels[vowelIndex])
+            {
+                vowelCounter++;
+            }
+        }
+    }
+
+    printf("\nThe word %s has %d vowels. ", word, vowelCounter);
+    return 0;
 }
