8e57549456b84a906dfb1fa6e3931114799ade9d
Matheus Carneiro <carneiromatheus943@gmail.com>
2026-07-20 14:53:31 -0300

    updated

--- 13_Team_bracket/team_bracket.c
+++ 13_Team_bracket/team_bracket.c
@@ -3,7 +3,7 @@
 
 int main()
 {
-    int i, teamCount, attackerIndex, defenderIndex;
+    int i, teamCount, attackerIndex, defenderIndex, positionIndex = 1;
 
     printf("How much teams will going to play?: \n");
     scanf("%d", &teamCount);
@@ -12,15 +12,17 @@ int main()
 
     for (i = 0; i < teamCount; i++)
     {
-        printf("Type the %d. team name?: ", i);
+        printf("Type the %d. team name?: ", positionIndex);
         scanf("%30s", teams[i]);
+        positionIndex++;
     }
+
     printf("\n");
     for (attackerIndex = 0; attackerIndex < teamCount; attackerIndex++)
     {
         for (defenderIndex = 0; defenderIndex < teamCount; defenderIndex++)
         {
-            if (strcasecmp(teams[attackerIndex], teams[defenderIndex]) != 0) 
+            if (strcasecmp(teams[attackerIndex], teams[defenderIndex]) != 0)
             {
                 printf("%s x %s\n", teams[attackerIndex], teams[defenderIndex]);
             }
