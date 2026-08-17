13a988c57c15826984a0a9813457c29d4bed0a3a
Matheus Carneiro <carneiromatheus943@gmail.com>
2026-08-17 17:15:37 -0300

    some corrections

--- 15_smokingdays.c
+++ 15_smokingdays.c
@@ -2,16 +2,16 @@
 
 int main()
 {
-    int amountDay, amountYears, minLife = 600, lifeSteal;
+    int amountDay, amountYears, minLife = 10;
 
-    printf("How much cigarette do you smoke per day?: \n");
+    printf("How many cigarette do you smoke per day?: \n");
     scanf("%d", &amountDay);
 
     printf("And how many years do you smoke?: \n");
     scanf("%d", &amountYears);
 
-    lifeSteal = (((((amountDay * minLife) * 365) * amountYears)/60)/60)/24;
+    float lifeSteal = ((amountDay * minLife * 365.0 * amountYears)/60)/24;
 
-    printf("Do lost %d days of your life smoking.", lifeSteal);
+    printf("You lose approximately %.2f days of your life smoking.", lifeSteal);
     return 0;
 }
